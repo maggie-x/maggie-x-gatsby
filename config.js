a129a2498d100451d09c39fc8171232834c8a1b4
maggie-x <z5162939@cse.unsw.edu.au>
2020-03-02 00:27:34 +1100

    add knowledge to path

--- config.js
+++ config.js
@@ -22,6 +22,10 @@ module.exports = {
     {
       label: 'Contact me',
       path: '/pages/contacts'
+    },
+    {
+      label: 'Knowledge',
+      path: '/pages/knowledge'
     }
   ],
   author: {
